3345e0d40ab3b92272883f36ce33a9578786f40a
TumasRackaitis <t.rack721@gmail.com>
2020-07-27 14:53:03 -0700

    Updated stippler.py as Scipy deprecated imread.
    Repo Branch fails as scipy.imread is deprecated. Followed instructions in scipy here: https://docs.scipy.org/doc/scipy-1.2.1/reference/generated/scipy.misc.imread.html to use Image.io. Imageio docs have article on how to transition from scipy, and I followed those steps: https://imageio.readthedocs.io/en/stable/scipy.html?highlight=flatten#transitioning-from-scipy-s-imread. 
    Love your work.

--- code/stippler.py
+++ code/stippler.py
@@ -45,6 +45,7 @@ import os.path
 import scipy.misc
 import scipy.ndimage
 import numpy as np
+import imageio
 
 def normalize(D):
     Vmin, Vmax = D.min(), D.max()
@@ -135,7 +136,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     filename = args.filename
-    density = scipy.misc.imread(filename, flatten=True, mode='L')
+    density = imageio.imread(filename, as_gray=True, pilmode='L')
 
     # We want (approximately) 500 pixels per voronoi region
     zoom = (args.n_point * 500) / (density.shape[0]*density.shape[1])
